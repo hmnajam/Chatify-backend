8ae764d85bff61f6876f1aa971b404027381bf6c
Najam <hmnajam@gmail.com>
2024-03-03 12:36:41 +0500

    no comment

--- appb.js
+++ appb.js
@@ -176,47 +176,6 @@ async function connectToWhatsApp() {
   }
 }
 
-// Function to retrieve all messages from MongoDB
-async function getAllMessagesFromDB() {
-  try {
-    const allMessages = await sentMessagesCollection
-      .find(
-        {},
-        { projection: { recipient: 1, message: 1, timestamp: 1, _id: 0 } }
-      )
-      .sort({ timestamp: -1 }) // Sort in descending order based on timestamp
-      .limit(20) // Limit the result to the last 20 messages
-      .toArray();
-    return allMessages;
-  } catch (error) {
-    console.error("Error retrieving messages from database:", error);
-    throw error;
-  }
-}
-
-app.get("/get-all-messages", async (req, res) => {
-  try {
-    const allMessages = await getAllMessagesFromDB();
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).send(
-      JSON.stringify(
-        {
-          status: true,
-          response: allMessages,
-        },
-        null,
-        4
-      )
-    );
-  } catch (error) {
-    res.status(500).json({
-      status: false,
-      response: "Error retrieving messages",
-    });
-  }
-  //
-});
-
 const isConnected = () => {
   return sock?.user ? true : false;
 };
@@ -327,6 +286,49 @@ app.get("/send-message", async (req, res) => {
   }
 });
 
+// Function to retrieve all messages from MongoDB
+async function getAllMessagesFromDB() {
+  try {
+    const allMessages = await sentMessagesCollection
+      .find(
+        {},
+        { projection: { recipient: 1, message: 1, timestamp: 1, _id: 0 } }
+      )
+      .sort({ timestamp: -1 }) // Sort in descending order based on timestamp
+      .limit(20) // Limit the result to the last 20 messages
+      .toArray();
+    return allMessages;
+  } catch (error) {
+    console.error("Error retrieving messages from database:", error);
+    throw error;
+  }
+}
+//
+//
+//
+app.get("/get-all-messages", async (req, res) => {
+  try {
+    const allMessages = await getAllMessagesFromDB();
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(
+      JSON.stringify(
+        {
+          status: true,
+          response: allMessages,
+        },
+        null,
+        4
+      )
+    );
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      response: "Error retrieving messages",
+    });
+  }
+});
+
+//
 io.on("connection", async (socket) => {
   soket = socket;
   if (isConnected()) {
---
+++ routes/home.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+// Home page
+router.get("/", (req, res) => {
+  const indexPath = path.join(__dirname, "../index.html");
+
+  res.sendFile(indexPath);
+  // Add other logic as needed
+});
+
+module.exports = router;
