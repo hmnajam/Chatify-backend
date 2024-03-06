48d6cbde1405973cbb067a4b12e7ebe8ac55c219
Najam <hmnajam@gmail.com>
2024-03-06 20:03:24 +0500

    C:/Program Files/Git/scan and -api-docs have some problems

--- appb.js
+++ appb.js
@@ -1,45 +1,40 @@
-const express = require("express");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
+const socketIO = require("socket.io");
 const app = require("express")();
-app.use(
-  fileUpload({
-    createParentPath: true,
-  })
-);
+const server = require("http").createServer(app);
+const io = socketIO(server); // Initialize socket.io with the server
+const port = process.env.PORT || 7000;
+
+// Middleware
+app.use(fileUpload({ createParentPath: true }));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/assets", express.static(__dirname + "/client/assets"));
-const server = require("http").createServer(app);
-const port = process.env.PORT || 7000;
-const socketIO = require("socket.io");
-const http = require("http");
-// const server = http.createServer(app);
-const io = socketIO(server); // Initialize socket.io with the server
-//
+
+//Routes
 const home = require("./routes/home");
-app.use("/", home);
 const scan = require("./routes/scan");
-app.use("/", scan);
+const allMessages = require("./routes/allMessages");
 const {
   router: sendMessageRouter,
   handleSocketConnection,
   connectToWhatsApp,
 } = require("./routes/sendMessage");
-app.use("/", sendMessageRouter);
+
 io.on("connection", handleSocketConnection);
 connectToWhatsApp().catch((err) =>
   console.log("Unexpected error in connecting to WhatsApp: " + err)
 );
-
-//
-const allMessages = require("./routes/allMessages");
-app.use("/", allMessages);
-
 const { swaggerUi, swaggerSpecs } = require("./config/swaggerConfig");
+
+// Using my routes
+app.use("/", home);
+app.use("/", scan);
+app.use("/", sendMessageRouter);
+app.use("/", allMessages);
 app.use(
   "/api-docs",
   swaggerUi.serve,
