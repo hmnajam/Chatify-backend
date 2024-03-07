13f31ba0f3581b16d89f159d90bc74966538832f
Najam <hmnajam@gmail.com>
2024-03-08 01:32:38 +0500

    Added .prettierrc

--- appb.js
+++ appb.js
@@ -1,45 +1,37 @@
-const bodyParser = require("body-parser");
-const app = require("express")();
-const server = require("http").createServer(app);
-const socketIO = require("socket.io");
+const bodyParser = require('body-parser');
+const app = require('express')();
+const server = require('http').createServer(app);
+const socketIO = require('socket.io');
 const io = socketIO(server); // Initialize socket.io with the server
-require("dotenv").config();
+require('dotenv').config();
 const port = process.env.PORT || 7000;
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-console.log("Middleware crossed.");
-// Routes
-const routes = require("./routes"); // Import simplified routes
+
+// // Importing Routes
+const routes = require('./routes');
 const { home, scan, allMessages, swaggerUi, swaggerSpecs } = routes;
-const {
-  router: sendMessageRouter,
-  handleSocketConnection,
-  connectToWhatsApp,
-} = require("./routes/sendMessage");
-io.on("connection", handleSocketConnection);
+const { router: sendMessageRouter, handleSocketConnection, connectToWhatsApp } = require('./routes/sendMessage');
+io.on('connection', handleSocketConnection);
 
 // WhatsApp Connection Success
 connectToWhatsApp()
   .then(() => {
-    console.log("Connected to WhatsApp successfully");
+    console.log('Connected to WhatsApp successfully');
   })
   .catch((err) => {
-    console.error("Error connecting to WhatsApp:", err);
+    console.error('Error connecting to WhatsApp:', err);
   });
 
 // Using simplified routes
-app.use("/", home);
-app.use("/", scan);
-app.use("/", sendMessageRouter);
-app.use("/", allMessages);
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerSpecs, { explorer: true })
-);
+app.use('/', home);
+app.use('/', scan);
+app.use('/', sendMessageRouter);
+app.use('/', allMessages);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs, { explorer: true }));
 
 server.listen(port, () => {
-  console.log("Server Running on Port : " + port);
+  console.log('Server Running on Port: ' + port);
 });
