b3ed8c18e8de31230cece08670ee3e7cf251301c
Najam <hmnajam@gmail.com>
2024-03-07 16:19:25 +0500

    Debugging

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const mongoURL = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}`;
-
+console.log('MOngo Url is ', mongoURL);
 // Defining mongoClient for mongodb connection
 const mongoClient = new MongoClient(mongoURL, {
   serverApi: {
